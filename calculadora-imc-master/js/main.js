8c90812b4949447108b8da71fc6600c6a5b2dfa3
poison <106964549+poisonrin@users.noreply.github.com>
2023-07-31 11:38:56 -0300

    Add files via upload

---
+++ calculadora-imc-master/js/main.js
@@ -0,0 +1,41 @@
+// Capturando os dados do formulário
+const dados = document.querySelector('#forms');
+
+// Criando um monitor do evento submit e função anônima
+dados.addEventListener('submit', function (event) {
+    
+    // Previne o evento padrão do submit
+    event.preventDefault();
+});
+    // Captura os dados dos inputs
+   
+
+
+    // Validade dos valores que atendem à condição
+    
+
+    // Chamada da função que calcula o IMC
+    
+
+    // Chamada da função que traz a mensagem da classificação do IMC
+   
+
+    // Monta a mensagem para ser enviada
+    
+
+    // Chama a função do resultado (mensagem) e seta como True
+    
+
+// Função que gera a classificação do IMC
+
+
+// Função para calcular o IMC
+
+
+// Função que cria o resultado
+
+
+// Função para criar o elemento p
+
+
+// Captura o evento 'reset' e limpa o resultado
